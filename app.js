557e2ed01f81dcd9fbb3a29c1041a06b01bb97f9
Stas Voronin <jameslebron@yandex.ru>
2021-07-11 19:17:12 +0300

    feat: подключил базу данных mongo

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const { PORT = 3000 } = process.env;
+
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
 
 app.listen(PORT, () => {
   console.log(`App listening ${PORT} port`);
