d3b59ddbbc36492932ed1e27a70c988bbdd67e10
Stas Voronin <jameslebron@yandex.ru>
2021-07-13 23:36:48 +0300

    feat: защитил роуты авторизацией

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const userRoutes = require('./users');
 const moviesRoutes = require('./movies');
 const { createUser, login } = require('../controllers/users');
+const auth = require('../middlewares/auth');
 
 router.post('/signup', createUser);
 router.post('/signin', login);
+router.use(auth);
 router.use('/users', userRoutes);
 router.use('/movies', moviesRoutes);
 
