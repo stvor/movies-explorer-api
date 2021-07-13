f24fdbbf1d814e361913fc1fe22a8c4e2b51a264
Stas Voronin <jameslebron@yandex.ru>
2021-07-13 22:54:42 +0300

    feat: добавил роуты signup и signin

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./users');
 const moviesRoutes = require('./movies');
+const { createUser, login } = require('../controllers/users');
 
+router.post('/signup', createUser);
+router.post('/signin', login);
 router.use('/users', userRoutes);
 router.use('/movies', moviesRoutes);
 
