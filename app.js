a0650541f1d410cf91e660150b097baccde3f4a8
Stas Voronin <jameslebron@yandex.ru>
2021-07-13 17:16:36 +0300

    feat: вынес настройки монги в конфиг

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { MONGO_URL } = require('./config');
 
 const app = express();
 const { PORT = 3000 } = process.env;
 
-mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
+mongoose.connect(MONGO_URL, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
